667738125655093503fa422cc06adbb196b08b5e
s-nakajima <nakajimashouhei@gmail.com>
2015-11-17 18:51:32 +0900

    ルームの権限チェック追加

---
+++ Controller/Component/PublicSpaceComponent.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * PublicSpace Component
+ *
+ * @author Noriko Arai <arai@nii.ac.jp>
+ * @author Shohei Nakajima <nakajimashouhei@gmail.com>
+ * @link http://www.netcommons.org NetCommons Project
+ * @license http://www.netcommons.org/license.txt NetCommons License
+ * @copyright Copyright 2014, NetCommons Project
+ */
+
+App::uses('Component', 'Controller');
+
+/**
+ * PublicSpace Component
+ *
+ * @author Shohei Nakajima <nakajimashouhei@gmail.com>
+ * @package NetCommons\PrivateSpace\Controller\Component
+ */
+class PublicSpaceComponent extends Component {
+
+/**
+ * アクセスチェック
+ *
+ * @param Controller $controller Controller with components to startup
+ * @return bool
+ */
+	public function accessCheck(Controller $controller) {
+CakeLog::debug('PublicSpaceComponent::accessCheck');
+		return true;
+	}
+}
